941acb0b8e7b7d147a9e4c65af627da788d0fe5d
Subena khatun <subenakhtun@gmail.com>
2024-10-26 17:53:12 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/list/list-create.py
@@ -0,0 +1,20 @@
+# What are lists in Python?
+''''
+** A list is a data structure in Python 
+**  mutable, or changeable, 
+** ordered sequence of elements. 
+** Each element or value that is inside of a list is called an item. 
+** lists are defined by having values 
+between square brackets [ ] 
+** It contains different data types: integer, float, string, object.
+'''
+# Create a list with veraious data types
+list1 = [12,30.5,'subena',{'subjectes': 'Mathematics'},[3,4]]
+# to display use print function
+print(f'Create a list: {list1}')
+
+# another list
+my_list = [1, 2, 3, 'Hello', [4, 5]]
+
+# Printing the created list
+print('Created List:', my_list)
