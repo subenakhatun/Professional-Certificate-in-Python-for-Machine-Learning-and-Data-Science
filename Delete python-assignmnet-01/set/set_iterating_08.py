ecaa37662d312890fef3062bd2f06d7be4be9dae
Subena khatun <subenakhtun@gmail.com>
2024-10-27 02:05:34 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/set/set_iterating_08.py
@@ -0,0 +1,8 @@
+# Example 9: Iterating Over a Set
+# You can iterate over a set to access its elements.
+# Since sets are unordered, the elements may be returned in any order.
+my_set = {'subena',30,10,20,30,78,50,62,89}
+my_set1 = {700,800,900,10,20,30,89}
+
+for i in my_set1:
+    print(i)
