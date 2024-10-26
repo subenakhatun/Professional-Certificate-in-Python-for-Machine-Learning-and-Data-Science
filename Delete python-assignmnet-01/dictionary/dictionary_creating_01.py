c24c7b417c5bd921252113a2d2586c3981999588
Subena khatun <subenakhtun@gmail.com>
2024-10-27 00:01:50 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/dictionary/dictionary_creating_01.py
@@ -0,0 +1,14 @@
+# Example 1: Creating a Dictionary
+# A dictionary is a collection of key-value pairs, created using curly braces {}.
+# Each key is unique, and it is used to access its corresponding value.
+
+# Creating dictionary
+my_dic = {
+    'name':['Subena','Shuly','Rahena','Farzana','Dhoha'],
+    'Subjects':['Future Data Scientist','Home maker','Dreamer','Hridoyban','just beginning her life'],
+    'Marks':[70,80,90,100,100],
+    'Age':[30,35,31,32,18],
+    'City':['Dhaka','Sylhet','Sylhet','Sylhet','Dhaka']       
+}
+# print the created Dictionary
+print(my_dic)
