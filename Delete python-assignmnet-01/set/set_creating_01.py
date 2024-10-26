c161a6419c1060ea17983ff37fa085f8665382ec
Subena khatun <subenakhtun@gmail.com>
2024-10-27 01:40:51 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/set/set_creating_01.py
@@ -0,0 +1,8 @@
+# Example 1: Creating a Set
+# A set is a collection of unique elements, created using curly braces {} or the set() function.
+# Sets are unordered, meaning that their elements do not have a specific order.
+# Unordered, Unchangeable,Duplicates Not Allowed
+
+# Create a set
+my_set = {'subena',30,10,20,30}
+print(my_set)
