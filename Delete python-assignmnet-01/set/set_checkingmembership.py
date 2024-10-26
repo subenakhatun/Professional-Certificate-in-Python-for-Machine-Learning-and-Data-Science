4c801568a0d78ea9741796285277ccea2f0c5255
Subena khatun <subenakhtun@gmail.com>
2024-10-27 02:10:47 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/set/set_checkingmembership.py
@@ -0,0 +1,9 @@
+# Example 8: Checking Membership in a Set
+# You can use the 'in' keyword to check if an element exists in a set.
+my_set = {'subena',30,10,20,30,78,50,62,89}
+my_set1 = {700,800,900,10,20,30,89}
+
+if 89 in my_set:
+    print('89 is in the set')
+else:
+    print('89 is not in the set')
