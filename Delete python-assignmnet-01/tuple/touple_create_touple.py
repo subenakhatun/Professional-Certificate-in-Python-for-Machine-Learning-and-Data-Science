cbbf045bffa829d23a8ad165cd1a8f49168baec9
Subena khatun <subenakhtun@gmail.com>
2024-10-26 22:17:33 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/tuple/touple_create_touple.py
@@ -0,0 +1,15 @@
+# Example: Creating tuple
+'''
+A tuple is a collection of items, similar to a list, but it is immutable (cannot be changed).
+
+Python tuples are a type of data structure that is very similar to lists. 
+The main difference between the two is that tuples are immutable, meaning they cannot be 
+changed once they are created. This makes them ideal for storing data that should not be modified, 
+such as database records.
+Tuple items are ordered, unchangeable, and allow duplicate values.
+Tuple items are indexed, the first item has index [0], the second item has index [1] etc
+Tuples are created using parentheses ().
+
+'''
+my_tuple = (24,56,'ML',[4,5,6],{'Subjects:':['Mathematics','Statistics']})
+print(my_tuple)
