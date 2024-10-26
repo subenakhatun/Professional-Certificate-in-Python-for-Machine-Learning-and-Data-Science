255a6530fe6d1d0eaff65c70d7c2c0662311e4af
Subena khatun <subenakhtun@gmail.com>
2024-10-27 00:41:22 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/dictionary/dictionary_lenghth_10.py
@@ -0,0 +1,14 @@
+# Example 10: Dictionary Length
+# The len() function is used to find the number of key-value pairs in a dictionary.
+
+# Creating dictionary
+my_dic = {
+    'name':['Subena','Shuly','Rahena','Farzana','Dhoha'],
+    'Subjects':['Future Data Scientist','Home maker','Dreamer','Hridoyban','just beginning her life'],
+    'Marks':[70,80,90,100,100],
+    'Age':[30,35,31,32,18],
+    'City':['Dhaka','Sylhet','Sylhet','Sylhet','Dhaka']       
+}
+
+# find the length
+print(len(my_dic))
---
+++ Delete python-assignmnet-01/dictionary/dictionary_mergening_09.py
@@ -0,0 +1,11 @@
+# Example 9: Merging Dictionaries
+# You can merge dictionaries using the update() method or the ** operator in Python 3.9+.
+# Example dictionaries
+dict1 = {'name': 'Subena', 'age': 30}
+dict2 = {'city': ' Dhaka', 'country': 'Bangladesh'}
+
+# Merging using update()
+dict1.update(dict2)
+
+# Printing the merged dictionary
+print('Merged Dictionary:', dict1)
