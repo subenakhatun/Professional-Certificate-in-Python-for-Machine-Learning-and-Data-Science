048b99a61ae2965061f5acc1313d05c10d056d68
Subena khatun <subenakhtun@gmail.com>
2024-10-27 00:31:28 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/dictionary/dictionar_KeyValuesItems_06.py
@@ -0,0 +1,16 @@
+
+
+my_dic = {
+    'name':['Subena','Shuly','Rahena','Farzana','Dhoha'],
+    'Subjects':['Future Data Scientist','Home maker','Dreamer','Hridoyban','just beginning her life'],
+    'Marks':[70,80,90,100,100],
+    'Age':[30,35,31,32,18],
+    'City':['Dhaka','Sylhet','Sylhet','Sylhet','Dhaka']       
+}
+
+allkeys = my_dic.keys()
+allvalues = my_dic.values()
+keyvaluespair = my_dic.items()
+print('All keys: ', allkeys)
+print('All values: ', allvalues)
+print('all key values pair: ', keyvaluespair)
