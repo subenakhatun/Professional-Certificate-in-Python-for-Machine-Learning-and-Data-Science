4fdce2c64ec821380aa5b786bbb4e76ebeb6f1c9
Subena khatun <subenakhtun@gmail.com>
2024-10-27 00:24:28 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/dictionary/dictionar_addingKeyValues_04.py
@@ -0,0 +1,17 @@
+# Example 4: Adding Key-Value Pairs to a Dictionary
+# You can add new key-value pairs to a dictionary by assigning a value to a new key.
+
+
+my_dic = {
+    'name':['Subena','Shuly','Rahena','Farzana','Dhoha'],
+    'Subjects':['Future Data Scientist','Home maker','Dreamer','Hridoyban','just beginning her life'],
+    'Marks':[70,80,90,100,100],
+    'Age':[30,35,31,32,18],
+    'City':['Dhaka','Sylhet','Sylhet','Sylhet','Dhaka']       
+}
+
+# Adding a new key-value pair
+my_dic['city'] = 'New York'
+
+# Printing the updated dictionary
+print('Updated Dictionary:', my_dic)
