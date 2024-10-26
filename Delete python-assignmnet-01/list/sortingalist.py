818d487f8801aa364cee7cb70a0e93128efcd7c0
Subena khatun <subenakhtun@gmail.com>
2024-10-26 19:03:39 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/list/sortingalist.py
@@ -0,0 +1,11 @@
+# Example 9: Sorting a List
+# The sort() method sorts a list in ascending order.
+# You can also sort in descending order by setting the reverse parameter to True.
+my_list = [34,40,55,80,96,35,70,29,85,49,70]
+print('as usal list',my_list)
+my_list.sort()
+print('sorted list',my_list)
+
+# again sort descending order
+my_list.sort(reverse=True)
+print('descending list',my_list)
