27e63ebe115e4840b46a24023f0b3bbb7567a858
Subena khatun <subenakhtun@gmail.com>
2024-10-26 22:46:14 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/tuple/tuple_length_10.py
@@ -0,0 +1,7 @@
+# Example 10: Tuple Length
+# The len() function is used to find the number of elements in a tuple.
+
+my_tuple = (24,56,'ML',[4,5,6],{'Subjects:':['Mathematics','Statistics']},100,300,400,34,56)
+
+# find the length of the tuple
+print('Number of iteams: ', len(my_tuple))
