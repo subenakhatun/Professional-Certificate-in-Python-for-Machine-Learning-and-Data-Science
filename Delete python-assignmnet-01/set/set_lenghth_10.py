7a9c9702cd0fde9eaeb362c6ffdec34739a10e12
Subena khatun <subenakhtun@gmail.com>
2024-10-27 02:07:06 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/set/set_lenghth_10.py
@@ -0,0 +1,4 @@
+# Example 10: Set Length
+# The len() function is used to find the number of elements in a set.
+my_set = {'subena',30,10,20,30,78,50,62,89}
+print('Length of the set: ',len(my_set))
