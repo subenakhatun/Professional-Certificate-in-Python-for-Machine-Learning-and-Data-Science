72c1f4be89f3d4bba0d385aaafd2c718e610da2c
Subena khatun <subenakhtun@gmail.com>
2024-10-26 18:49:51 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/list/removElementFromtheList.py
@@ -0,0 +1,12 @@
+# You can remove elements from a list using remove() and pop(). using postion no
+# remove() deletes the first occurrence of a value, while pop() removes an element by index.
+
+my_list = [34,40,55,80,96,35,70,29,85,49,70]
+# remove an element
+my_list.remove(70)
+
+# popped element 
+my_list.pop(3)
+
+#  list after remove and pop
+print(my_list)
