2f67a9781ee4aaea39eb2629dd057932e6dc81a1
Subena khatun <subenakhtun@gmail.com>
2024-10-26 23:14:00 +0600

    Delete Delete python-assignmnet-01/listLenght.py

--- Delete python-assignmnet-01/listLenght.py
+++
@@ -1,7 +0,0 @@
-# Example 10: List Length
-# The len() function is used to find the number of elements in a list.
-
-my_list = [34,40,55,80,96,35,70,29,85,49,70]
-
-# find the list of the length using len() function
-print('lenght of a list is: ',len(my_list))
