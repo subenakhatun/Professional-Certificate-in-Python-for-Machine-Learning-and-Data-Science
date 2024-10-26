cc7528684e2787926e32c6ac6b386dabeac319aa
Subena khatun <subenakhtun@gmail.com>
2024-10-26 19:00:15 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/list/listChekingMembership.py
@@ -0,0 +1,18 @@
+# Example 8: Checking Membership in a List
+# You can use the 'in' keyword to check if an element exists in a list.
+
+my_list = [34,40,55,80,96,35,70,29,85,49,70]
+
+if 40 in my_list:
+    print('available')
+else:
+    print('not availabel')
+
+# Example list
+my_list = ['apple', 'banana', 'cherry']
+
+# Checking for membership
+if 'banana' in my_list:
+    print('Banana is in the list')
+else:
+    print('Banana is not in the list')
