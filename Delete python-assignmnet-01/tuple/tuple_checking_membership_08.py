eeecb09a7bf9730bd22dff2675885fad065e2ffc
Subena khatun <subenakhtun@gmail.com>
2024-10-26 23:03:56 +0600

    Add files via upload

---
+++ Delete python-assignmnet-01/tuple/tuple_checking_membership_08.py
@@ -0,0 +1,9 @@
+# Example 8: Checking Membership in a Tuple
+# You can use the 'in' keyword to check if an element exists in a tuple.
+
+my_tuple = (24,56,'ML',[4,5,6],{'Subjects:':['Mathematics','Statistics']},100,300,400,34,56)
+
+if 24 in my_tuple:
+    print(f"Yes 24 is availabe.")
+else:
+    print('Not found')
