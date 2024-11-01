d229dc5a4168ab9ad7500d8479c14e69041f8a68
Subena khatun <subenakhtun@gmail.com>
2024-11-02 03:18:18 +0600

    cretae file

---
+++ 10PythonSamplesAssignmnet/dict.py
@@ -0,0 +1,11 @@
+
+# Example 9: Dictionary Filtering Based on Conditions
+# Filtering a dictionary to retain only key-value pairs that satisfy a condition.
+
+students_marks = {
+'Name':['Barlowe','Caddel','Madden'],
+'Subjects': ['English','Computer Science','Physics'],
+'Marks': [80,94,86],
+'Passing Year':[2021,2022,2023]                         
+}
+print(students_marks)
