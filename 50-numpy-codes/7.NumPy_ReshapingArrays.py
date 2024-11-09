872e7de3a0c790c1e10539c105ebeeb56867ae28
Subena khatun <subenakhtun@gmail.com>
2024-11-09 21:50:02 +0000

    updated

--- 50-numpy-codes/7.NumPy_ReshapingArrays.py
+++ 50-numpy-codes/7.NumPy_ReshapingArrays.py
@@ -3,3 +3,5 @@ import numpy as np
 array_1 = np.array([1, 2, 3, 4, 5, 6])
 reshape_array = array_1.reshape((3,2)) # 3 rows and 2 columns: total 6 values distribution
 print(f'Cobvert 2D from 1D {reshape_array}')
+array_2 = np.array([1, 2, 3, 4, 5, 6,7,8])
+print(f'cobvert 3D from 1D {array_2.reshape((2,2,2))}')
