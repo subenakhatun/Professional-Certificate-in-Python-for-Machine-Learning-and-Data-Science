3977f3b2394c7dca00e6d0d77bf2441e39e814b8
Subena khatun <subenakhtun@gmail.com>
2024-11-09 19:30:37 +0000

    upadated code

--- 50-numpy-codes/2.py
+++ 50-numpy-codes/2.py
@@ -8,3 +8,12 @@ print(array_2d)
 # Accessing individuals elements. Indexing start form 0 not 1
 # 2 no row and 2 no columns value
 print(f' 2 no row and 2 no colunm values: {array_2d[1,1]}')
+
+# Slicing rows and columns
+# first row and all columns
+print(f' 1st row and all colunms value: {array_2d[0, :]}')
+# 3rd  row and all columns
+print(f' 1st row and all colunms value: {array_2d[2, :]}')
+
+# all rows but 3rd columns
+print(f' every rows, 3rd column values: {array_2d[:, 2]}')
