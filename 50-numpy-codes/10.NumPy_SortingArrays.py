07512f300420e3e2d849c7feed1487695e8ff3a4
Subena khatun <subenakhtun@gmail.com>
2024-11-09 22:21:49 +0000

    update

---
+++ 50-numpy-codes/10.NumPy_SortingArrays.py
@@ -0,0 +1,11 @@
+# Example 10: NumPy - Sorting Arrays
+import numpy as np
+
+# Creating an array
+array = np.array([3, 1, 2, 5, 4])
+
+# Sorting the array: ascending to decending
+sorted_array = np.sort(array)
+
+print(f'Original Array: {array}')
+print(f'Sorted Array:{sorted_array}')
