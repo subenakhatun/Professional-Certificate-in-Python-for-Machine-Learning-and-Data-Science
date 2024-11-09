78250b502e5ff95c36705c97f80c14cc0a8110a7
Subena khatun <subenakhtun@gmail.com>
2024-11-09 20:23:44 +0000

    create log python and run it

---
+++ 50-numpy-codes/4.NumPy_ExponentialAndLogarithmicFunctions.py
@@ -0,0 +1,7 @@
+# Exponent and logarithm: np.log(array), np.exp(array)
+import numpy as np
+array_1 = np.array([10,20,30,40,50,60,70,80,90,50,60])
+
+exponential_values = np.exp(array_1)
+logarithm_values = np.log(array_1)
+print(f'Exponential values {exponential_values} and logarithm values is {logarithm_values}')
