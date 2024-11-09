e9d458c527299180161ee2c0bcce178318899d7e
Subena khatun <subenakhtun@gmail.com>
2024-11-09 08:52:55 +0600

    change commit

--- 10PythonSamplesAssignmnet/DictionaryMergingWithComprehension.py
+++ 10PythonSamplesAssignmnet/DictionaryMergingWithComprehension.py
@@ -5,8 +5,10 @@ dict1 = {'a': 1, 'b': 2, 'c': 3}
 dict2 = {'b': 4, 'c': 5, 'd': 6}
 
 # Merging and modifying values
+# merge 2 sets keys
 dict3 = set(dict1) | set(dict2)
 print(dict3)
+
 # merged_dict = {key: dict1.get(key, 0) + dict2.get(key, 0) for key in set(dict1) | set(dict2)}
 
 # print('Merged and Modified Dictionary:', merged_dict)
