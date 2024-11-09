50701152d7f099de411314ca443c8d19039683d4
Subena khatun <subenakhtun@gmail.com>
2024-11-09 20:31:54 +0000

    print and updated

--- 50-numpy-codes/5.NumPy_SlicingWithStep.py
+++ 50-numpy-codes/5.NumPy_SlicingWithStep.py
@@ -4,3 +4,9 @@ array_1 = np.array([10,20,30,40,50,60,70,80,90,50,60])
 
 # show 1st to last values every 2 steps
 print(f'Show 1st to last all values but all odd no values: {array_1[::2]}')
+
+# show from last to 1st values every 2 step
+print(f'Show 1st to last all values but all odd no values: {array_1[::-2]}')
+
+# start from 2 and end but 2 step
+print(f'Show 1st to last all values but all odd no values: {array_1[2::2]}')
