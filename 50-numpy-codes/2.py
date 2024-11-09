5343d7c065ff13581989819672bb431f2d1f2e30
Subena khatun <subenakhtun@gmail.com>
2024-11-09 19:25:49 +0000

    print values

--- 50-numpy-codes/2.py
+++ 50-numpy-codes/2.py
@@ -5,3 +5,6 @@ array_2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
 # print and show is it 2d or none
 print(array_2d)
 
+# Accessing individuals elements. Indexing start form 0 not 1
+# 2 no row and 2 no columns value
+print(f' 2 no row and 2 no colunm values: {array_2d[1,1]}')
