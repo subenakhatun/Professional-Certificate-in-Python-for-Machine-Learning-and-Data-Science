30e6e616d6261388f0a9a19791199756130e9e59
Subena khatun <subenakhtun@gmail.com>
2024-11-09 19:48:02 +0000

    create an array

---
+++ 50-numpy-codes/1.NumPy_BasicIndexingAndSlicing.py
@@ -0,0 +1,8 @@
+# Creating an array
+
+import numpy as np
+array_1 = np.array([1,2,3,4,5,5,6,7,8])
+print(array_1)
+
+# accessing elements by index: index start from 0
+print(f'Print 2 no position value: {array_1[3]}')
