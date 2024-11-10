db2bcf420fce8c423b5f11b63b08940cc670a442
Subena khatun <subenakhtun@gmail.com>
2024-11-10 20:53:27 +0000

    updated

--- 50-numpy-codes/13.NumPy_StatisticalFunctions.py
+++ 50-numpy-codes/13.NumPy_StatisticalFunctions.py
@@ -1,6 +1,9 @@
-# NumPy - Statistical Functions: np.mean(arr)/median(arr)/mode(arr)/std(arr)/var(arr)
+# NumPy - Statistical Functions: np.mean(arr)/median(arr)/std(arr)/var(arr)
 import numpy as np
 
 # Creating an array
 array_1 = np.array([1, 2, 3, 4, 5])
 print(f'Mean of array: {np.mean(array_1)}')
+print(f'Median of array: {np.median(array_1)}')
+print(f'Standard Deviation of array: {np.std(array_1)}')
+print(f'Varience of array: {np.var(array_1)}')
