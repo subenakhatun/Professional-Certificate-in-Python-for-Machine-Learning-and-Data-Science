d89d5f3709aa515baa19758c7cd5efc61f25624d
Subena khatun <subenakhtun@gmail.com>
2024-11-10 21:18:20 +0000

    updated

--- 50-numpy-codes/16.NumPy_ExtractingDiagonalElements.py
+++ 50-numpy-codes/16.NumPy_ExtractingDiagonalElements.py
@@ -5,6 +5,7 @@ import numpy as np
 matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(f'Main Matrix: \n {matrix}')
 print(f'every 1 columns rows values: \n {matrix[0:,-1:]}')
+print(f'just show me 6 values: \n {matrix[1,2]}')
 # Extracting the main diagonal
 diagonal_elements = np.diag(matrix)
 
