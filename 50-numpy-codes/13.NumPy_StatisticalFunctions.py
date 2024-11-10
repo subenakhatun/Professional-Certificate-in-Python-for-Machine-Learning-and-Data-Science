066897e061e5085cd43e5b577e054fb50513e43b
Subena khatun <subenakhtun@gmail.com>
2024-11-10 20:51:50 +0000

    create new file and write code

---
+++ 50-numpy-codes/13.NumPy_StatisticalFunctions.py
@@ -0,0 +1,6 @@
+# NumPy - Statistical Functions: np.mean(arr)/median(arr)/mode(arr)/std(arr)/var(arr)
+import numpy as np
+
+# Creating an array
+array_1 = np.array([1, 2, 3, 4, 5])
+print(f'Mean of array: {np.mean(array_1)}')
