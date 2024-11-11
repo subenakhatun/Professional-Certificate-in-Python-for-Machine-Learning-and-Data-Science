5d0b49365accd3cf19c6143571973ca6a1a1503a
Subena khatun <subenakhtun@gmail.com>
2024-11-11 20:30:08 +0000

    updated

---
+++ 50-numpy-codes/21.NumPy_ReverseArrayUsingIndexing.py
@@ -0,0 +1,8 @@
+# NumPy - Reverse Array using Indexing
+import numpy as np
+
+# Creating an array
+array = np.array([1, 2, 3, 4, 5])
+print(f'Orginal Array. {array}')
+print(f'Reverse a array. {array[::-1]}')
+# print(f'Reverse a array. {array[-1::]}')
