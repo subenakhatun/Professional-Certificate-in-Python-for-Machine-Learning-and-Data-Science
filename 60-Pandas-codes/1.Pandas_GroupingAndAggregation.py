9740f6d940269cba0b9620d573ce77eea473d24b
Subena khatun <subenakhtun@gmail.com>
2024-11-11 22:02:58 +0000

    created a folder and created a file of pandas

---
+++ 60-Pandas-codes/1.Pandas_GroupingAndAggregation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Creating a DataFrame
+data = {'Category': ['A', 'B', 'A', 'B', 'C', 'C', 'A'],
+        'Value': [10, 20, 15, 25, 35, 45, 30]}
+df = pd.DataFrame(data)
+
+# Grouping by 'Category' and calculating the sum and mean
+grouped = df.groupby('Category').agg({'Value': ['sum', 'mean']})
+
+print('Grouped DataFrame with Aggregations:\n', grouped)
