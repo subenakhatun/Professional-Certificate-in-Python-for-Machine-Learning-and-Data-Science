b62cb4af50b210b48656caf203ec6d176423963c
Subena khatun <subenakhtun@gmail.com>
2024-11-26 13:12:28 +0000

    upadated

--- 60-Pandas-codes/10.Pandas_CustomCategoricalData.py
+++ 60-Pandas-codes/10.Pandas_CustomCategoricalData.py
@@ -6,6 +6,6 @@ data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'], 'Score': [85, 70, 95, 60]}
 df = pd.DataFrame(data)
 
 # Creating a new column with categorical data
-df['Performance'] = pd.cut(df['Score'], bins=[0, 70, 90, 100], labels=['Poor', 'Average', 'Excellent'])
+df['Performance'] = pd.cut(df['Score'], bins=[0, 70, 90, 100], labels=['A', 'V', 'C'])
 
 print('DataFrame with Categorical Performance:\n', df)
