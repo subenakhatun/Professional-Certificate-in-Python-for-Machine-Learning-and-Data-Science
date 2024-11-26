da9509ac4013f283bb8319e18741a12e2679fdfc
Subena khatun <subenakhtun@gmail.com>
2024-11-26 14:16:56 +0000

    updated code

--- 60-Pandas-codes/12.Pandas_ShiftAndLagging.py
+++ 60-Pandas-codes/12.Pandas_ShiftAndLagging.py
@@ -7,6 +7,10 @@ data = {'Date': pd.date_range(start='2023-01-01', periods=5, freq='D'),
 df = pd.DataFrame(data)
 
 # Shifting data by one day to create a lag
-df['Prev Day Temp'] = df['Temperature'].shift(1)
+# create new colum previsous day then i can use shift(position no start from here as like)
+df['Prev Day Temp'] = df['Temperature'].shift(2)
 
 print('DataFrame with Shifted Data:\n', df)
+
+df['Privious Prevoius day Temperature'] = df['Temperature'].shift(2)
+print('\n',df)
