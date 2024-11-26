3d18fa7242eff48f3be7a813f793bc2ced0e0bfd
Subena khatun <subenakhtun@gmail.com>
2024-11-26 09:28:05 +0000

    confused

--- 60-Pandas-codes/1.Pandas_GroupingAndAggregation.py
+++ 60-Pandas-codes/1.Pandas_GroupingAndAggregation.py
@@ -4,8 +4,10 @@ import pandas as pd
 data = {'Category': ['A', 'B', 'A', 'B', 'C', 'C', 'A'],
         'Value': [10, 20, 15, 25, 35, 45, 30]}
 df = pd.DataFrame(data)
-
+grouped = df.groupby('Category').agg(sum)
+print(grouped)
 # Grouping by 'Category' and calculating the sum and mean
 grouped = df.groupby('Category').agg({'Value': ['sum', 'mean']})
 
-print('Grouped DataFrame with Aggregations:\n', grouped)
+# print('Grouped DataFrame with Aggregations:\n', grouped)
+# confused
